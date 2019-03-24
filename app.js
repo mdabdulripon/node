3c370cd1e63e49a2a83a0c4bd6984e79371aeb61
mdabdulripon <mdabdulripon@gmail.com>
2019-03-24 07:26:18 -0400

    explore the path module.

--- app.js
+++ app.js
@@ -1,29 +1,24 @@
-const _http = require('http');
-const Person = require('./person');
+const _path = require('path');
 
 
-const parson1 = new Person('john doe', 30)
-console.log(`parson1 :`, parson1);
 
-const server = _http.createServer((req, res, next) => {
-    // ! Request
-    console.log(`request url :`, req.url);
-    console.log(`request headers :`, req.headers);
-    console.log(`request method :`, req.method);
+console.log(`*********************Path Module***********************`);
+console.log(`_path :`, _path);
+console.log(`*********************Path Module***********************`);
 
-    // ! Response
-    res.setHeader('Content-Type', 'text/html');
-    res.write('<html>');
 
-        res.write('<head>');
-            res.write('<title>Node js</title>');
-        res.write('</head>');
 
-        res.write('<body>');
-            res.write('<p>Welcome to Node js app</p>');
-        res.write('</body>');
 
-    res.write('</html>');
-});
+const _basename = _path.basename(__filename);
+console.log('_basename : ', _basename);
 
-server.listen(3000);
+
+const _extname = _path.extname(__filename);
+console.log('_extname : ', _extname)
+
+
+
+const _parse = _path.parse(__filename);
+console.log('_parse : ', _parse)
+
+
